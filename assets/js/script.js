3c242a2c89b52878c7d15e339214e372587045cf
MrTofuuu <chris.stallcup@gmail.com>
2021-09-14 12:21:39 -0500

    adding api link for what may be the more optimal use case for hw

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,8 @@ var formSubmitHandler = function(event) {
 
 var getCityWeather = function(city) {
     var apiUrl = 'api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=f48eeb974e0cd19636dc2234eda9e443';
+
+    //api.openweathermap.org/data/2.5/forecast/daily?q={city name},{state code},{country code}&cnt={cnt}&appid={API key} this might be the more correct use case for the hw
     //f48eeb974e0cd19636dc2234eda9e443 api key
     fetch(apiUrl)
         .then(function(response) {
